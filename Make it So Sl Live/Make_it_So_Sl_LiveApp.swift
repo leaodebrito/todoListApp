4b7008308c3cc600bbc1629090bd6d9ccd0ba182
Bruno Leão <leaodebrito@icloud.com>
2021-05-12 05:34:22 -0300

    Initial Commit

---
+++ Make it So Sl Live/Make_it_So_Sl_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_it_So_Sl_LiveApp.swift
+//  Make it So Sl Live
+//
+//  Created by Bruno Brito on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Make_it_So_Sl_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
